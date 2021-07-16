a52780530a38ff7144b7afb0f1c942c18600f691
Dustin <murdockdm@outlook.com>
2021-07-15 21:06:45 -0600

    added comment

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import {startPeer} from './peer/index';
 import {startWS} from './ws/index';
 
 const main = async () => {
+    //start the libp2p peer
     const peer = await startPeer();
     const ws = startWS(peer.pubsub);
     ws.on('close', async () => {
